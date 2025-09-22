ac053e3c05937dd96216250d30cddffdeae96fa6
vb1980 <vb1980@sina.com>
2025-09-22 16:37:32 +0800

    update build_next.sh

--- scripts/build_next.sh
+++ scripts/build_next.sh
@@ -3,6 +3,7 @@ ROOTDIR=$(cd `dirname $0`; pwd)
 TOOLCHAINSDIR=$ROOTDIR/mtk-toolchains
 SRCDIR=$ROOTDIR/asuswrt-modx-next/release/src-ra-openwrt-4210
 
+sudo ln -s /usr/bin/gettext-runtime/autopoint /usr/bin/autopoint
 cd $TOOLCHAINSDIR
 sudo ln -sf $(pwd)/toolchain-aarch64_cortex-a53+neon-vfpv4_gcc-5.4.0_glibc-2.24 /opt/
 sudo ln -sf $(pwd)/lede-toolchain-mediatek-mt7629_gcc-5.4.0_musl-1.1.24_eabi.Linux-x86_64 /opt/
